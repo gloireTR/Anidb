4464e797ac2c8606887b47596b1d1ebe9c8300b9
Sefa <70075198+gloireTR@users.noreply.github.com>
2020-10-22 22:00:36 +0300

    Update creators.php
    Bug fixed.

--- src/creators/creators.php
+++ src/creators/creators.php
@@ -111,11 +111,11 @@ class Creators extends Options{
         $control = '';
         preg_match_all(parent::PATTERN_THIRD_CREATOR_ANIMES_ONE . $this->creator_id . parent::PATTERN_SECOND_CREATOR_ANIMES, $this->html, $arr);
         if(!isset($arr[2][0])){
-            preg_match_all(parent::PATTERN_THIRD_CREATOR_ANIMES_ONE . $this->creator_id . parent::PATTERN_THIRD_CREATOR_ANIMES_TWO, $html, $arr);
+            preg_match_all(parent::PATTERN_THIRD_CREATOR_ANIMES_ONE . $this->creator_id . parent::PATTERN_THIRD_CREATOR_ANIMES_TWO, $this->html, $arr);
             if (isset($arr[2][0])){
                 $dom->loadHTML($arr[2][0]);
             }else{
-                preg_match_all(parent::PATTERN_FOURTH_CREATOR_ANIMES . $this->creator_id . parent::PATTERN_THIRD_CREATOR_ANIMES_TWO, $html, $arr);
+                preg_match_all(parent::PATTERN_FOURTH_CREATOR_ANIMES . $this->creator_id . parent::PATTERN_THIRD_CREATOR_ANIMES_TWO, $this->html, $arr);
                 if (isset($arr[2][0])){
                     $dom->loadHTML($arr[2][0]);
                 }else{
